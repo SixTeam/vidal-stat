212c36693eb160372757c6d0e474e9f3037717e5
ubourdon <bourdon.ugo@gmail.com>
2013-05-23 16:01:45 +0200

    add: stub ping controller

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,11 +2,39 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import model.Ping
 
 object Application extends Controller {
-  
-  def index = Action {
-    Ok(views.html.index("Your new application is ready."))
-  }
-  
+
+    var pings = List[Ping]()
+
+    def index = Action {
+        Ok(views.html.index("Your new application is ready."))
+    }
+
+    /*String product,
+    String version,
+    String ccVersion,
+    String ccuuid,
+    String login,
+    String pingType,
+    String detectedOs)*/
+
+    // PUT /ping?version=1&ccVersion=2&ccuid=112233&login=toto&pingType=ping&detectedOs=genou
+    def ping = Action { request =>
+        val version = request.getQueryString("version")
+        val ccVersion = request.getQueryString("ccVersion")
+        val ccuid = request.getQueryString("ccuid")
+        val login = request.getQueryString("login")
+        val pingType = request.getQueryString("pingType")
+        val detectedOs = request.getQueryString("detectedOs")
+
+        pings = Ping(version, ccVersion, ccuid, login, pingType, detectedOs) :: pings
+
+        Ok("ping received")
+    }
+
+    def showPings = Action { Ok(pings.toString()) }
+
+    def clear = Action { pings = Nil; Ok(pings.toString()) }
 }
--- project/Build.scala
+++ project/Build.scala
@@ -4,18 +4,18 @@ import play.Project._
 
 object ApplicationBuild extends Build {
 
-  val appName         = "vidal-stat"
-  val appVersion      = "1.0-SNAPSHOT"
+    val appName = "vidal-stat"
+    val appVersion = "1.0-SNAPSHOT"
 
-  val appDependencies = Seq(
-    // Add your project dependencies here,
-    jdbc,
-    anorm
-  )
+    val appDependencies = Seq(
+        "org.mongodb" %% "casbah" % "2.5.1",
+        "org.scalatest" %% "scalatest" % "1.9.1" % "test",
+        "com.github.simplyscala" %% "scalatest-embedmongo" % "0.2.1" % "test"
+    )
 
 
-  val main = play.Project(appName, appVersion, appDependencies).settings(
-    // Add your own project settings here      
-  )
+    val main = play.Project(appName, appVersion, appDependencies).settings(
+        // Add your own project settings here
+    )
 
 }
